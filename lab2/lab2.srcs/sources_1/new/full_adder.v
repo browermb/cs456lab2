c123c6a073ad5eae8e32e8134c56a8af0ddcc2fd
Brower <browermb@l24604.cs.jmu.edu>
2020-02-19 09:59:25 -0500

    implemented full adder

--- lab2/lab2.srcs/sources_1/new/full_adder.v
+++ lab2/lab2.srcs/sources_1/new/full_adder.v
@@ -7,7 +7,14 @@ module full_adder (
 );
     // Insert your RTL here to calculate the sum and carry out bits
     // Remove these assign statements once you write your own RTL
+    wire aXORb;
+    wire aANDb;
+    wire xorAndcin;
     
-    assign sum = 1'b0;
-    assign carry_out = 1'b0;
+    xor(aXORb, a, b);
+    and(aANDb, a, b);
+    and(xorAndcin, aXORb, carry_in);
+    or(carry_out, xorANDcin, aANDb);
+    xor(sum, aXORb, carry_in);
+
 endmodule
--- lab2/lab2.srcs/sources_1/new/structural_adder.v
+++ lab2/lab2.srcs/sources_1/new/structural_adder.v
@@ -3,5 +3,7 @@ module structural_adder (
     input [13:0] b,
     output [14:0] sum
 );
+    wire a0, b0;
+    wire a1, b1;
     assign sum = 15'd0;
 endmodule
